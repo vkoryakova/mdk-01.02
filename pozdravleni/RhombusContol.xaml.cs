16266e7fe027f8f1056c4fcdd35fe96dab10f2d5
Корякова Виктория <118850468+vkoryakova@users.noreply.github.com>
2023-05-03 14:42:48 +0300

    Add files via upload

---
+++ pozdravleni/RhombusContol.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pozdravleni
+{
+    /// <summary>
+    /// Логика взаимодействия для RhombusContol.xaml
+    /// </summary>
+    public partial class RhombusContol : UserControl
+    {
+        public string Header { get; set; }
+        public string Description { get; set; }
+        public ImageSource ColoredSource { get; set; }
+        public ImageSource BlackWhiteSource =>
+            (ColoredSource == null) ? null : new FormatConvertedBitmap((BitmapSource)ColoredSource, PixelFormats.Gray8, null, 0);
+        public RhombusContol()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void BtnAction_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(Description, Header, MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
