99b1c22c19b37636d5a1e6e5ced7d4a06960a98f
Корякова Виктория <118850468+vkoryakova@users.noreply.github.com>
2023-05-07 16:39:16 +0300

    Add files via upload

---
+++ Помещения/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using RoomLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Помещения
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Room> lstRooms= new List<Room>(); //создание списка комнат
+      
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TBLengthR_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void BAddRoom_Click(object sender, RoutedEventArgs e)
+        {
+            Room room = new Room();
+            room.RoomLength = Convert.ToDouble(TBLengthR.Text);
+            room.RoomWidth= Convert.ToDouble(TBWidthR.Text);
+            lstRooms.Add(room); //доавление в список
+        }
+
+        private void BAddLivingRoom_Click(object sender, RoutedEventArgs e)
+        {
+            LivingRoom livingRoom = new LivingRoom();
+            livingRoom.RoomLength = Convert.ToDouble(TBLengthL.Text);
+            livingRoom.RoomWidth = Convert.ToDouble(TBWidthL.Text);
+            livingRoom.NumWin = Convert.ToInt32(TBNumW.Text);
+            lstRooms.Add(livingRoom);
+        }
+
+        private void BAddOffice_Click(object sender, RoutedEventArgs e)
+        {
+            Office office = new Office();
+            office.RoomLength = Convert.ToDouble(TBLengthO.Text);
+            office.RoomWidth = Convert.ToDouble(TBWidthO.Text);
+            office.NumSockets = Convert.ToInt32(TBNumS.Text);
+            lstRooms.Add(office);
+        }
+
+        private void ListRooms_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void BGetList_Click(object sender, RoutedEventArgs e)
+        {
+            ListRooms.Content = "";
+            foreach (Room r in lstRooms)
+                    ListRooms.Content += r.Info() + "\n";
+            
+        }
+
+        private void ListRooms_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+           
+        }
+    }
+}
